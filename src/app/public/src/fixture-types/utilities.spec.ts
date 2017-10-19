3c8747ad4e89bcc5d5b5e1cf4d6b4d23fd793c51
Paul Crowder <paul.crowder@blackbaud.com>
2017-10-19 15:15:33 -0400

    Check for undefined elements

--- src/app/public/src/fixture-types/utilities.spec.ts
+++ src/app/public/src/fixture-types/utilities.spec.ts
@@ -9,12 +9,39 @@ import {
 describe('Utilities', () => {
 
   let bgEl: HTMLDivElement;
+  let textEl: HTMLSpanElement;
 
   beforeEach(() => {
     bgEl = document.createElement('div');
+    textEl = document.createElement('span');
 
     document.body.appendChild(bgEl);
+    document.body.appendChild(textEl);
+  });
 
+  afterEach(() => {
+    document.body.removeChild(bgEl);
+    document.body.removeChild(textEl);
+  });
+
+  it('should provide a method for determining if an element is visible', () => {
+    expect(SkyTestFixtureUtilities.isVisible(textEl)).toBe(true);
+
+    textEl.style.display = 'none';
+
+    expect(SkyTestFixtureUtilities.isVisible(textEl)).toBe(false);
+
+    expect(SkyTestFixtureUtilities.isVisible(undefined)).toBeUndefined();
+  });
+
+  it('should provide a method for retrieving an element\'s inner text', () => {
+    expect(SkyTestFixtureUtilities.getText(textEl)).toBe('');
+
+    textEl.innerText = '    test   ';
+
+    expect(SkyTestFixtureUtilities.getText(textEl)).toBe('test');
+
+    expect(SkyTestFixtureUtilities.getText(undefined)).toBeUndefined();
   });
 
   it('should provide a method for retrieving an element\'s background URL', () => {
@@ -35,6 +62,10 @@ describe('Utilities', () => {
     );
 
     expect(imageUrl).toBe('https://example.com/bg/');
+
+    imageUrl = SkyTestFixtureUtilities.getBackgroundImageUrl(undefined);
+
+    expect(imageUrl).toBeUndefined();
   });
 
 });
